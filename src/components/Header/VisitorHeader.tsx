6376e8e7dc6b825e7bc326fa961a08b2ace050eb
Ahmed Idrissi <83851898+ahmedidrissi@users.noreply.github.com>
2024-08-27 19:57:21 +0100

    Include Sign In and Sign Up links for small screens

--- src/components/Header/VisitorHeader.tsx
+++ src/components/Header/VisitorHeader.tsx
@@ -75,11 +75,35 @@ const VisitorHeader = ({ sticky }) => {
           </div>
 
           <nav
-            className={`absolute right-0 top-16 bg-white dark:bg-dark rounded-md shadow-lg ${
+            className={`absolute right-0 top-16 py-4 bg-white dark:bg-dark rounded-md shadow-lg ${
               menuOpen ? "block" : "hidden"
             } transform transition-transform duration-300 lg:flex lg:relative lg:top-0 lg:bg-transparent lg:dark:bg-transparent lg:shadow-none`}
           >
             <ul className="block lg:flex lg:space-x-2">
+              {/* Sign In and Sign Up links for small screens */}
+              <li className="relative">
+                <Link href="/signin" onClick={() => setMenuOpen(false)} className={`lg:hidden mx-4 mb-2 rounded-sm border flex py-2 px-4 text-base ${
+                  pathname === "/signin"
+                    ? "text-primary dark:text-white"
+                    : "text-dark hover:text-primary dark:text-white/70 dark:hover:text-white"
+                }`}>
+                  Sign In
+                </Link>
+              </li>
+              <li className="relative">
+                <Link href="/signup" onClick={() => setMenuOpen(false)} className={`lg:hidden mx-4 mb-4 rounded-sm bg-primary flex py-2 px-4 text-base ${
+                  pathname === "/signup"
+                    ? "text-primary dark:text-white"
+                    : "text-dark hover:text-primary dark:text-white/70 dark:hover:text-white"
+                }`}>
+                  Sign Up
+                </Link>
+              </li>
+              {/* Divider */}
+              <li className="lg:hidden">
+                <hr className="h-0.5 w-full bg-dark dark:bg-white/10 my-2" />
+              </li>
+              {/*  Nav Items */}
               {navItems.map((item) => (
                 <li key={item.href} className="relative">
                   <Link href={item.href} onClick={() => setMenuOpen(false)} className={`flex py-2 px-4 text-base ${
@@ -96,14 +120,14 @@ const VisitorHeader = ({ sticky }) => {
 
       
           <div className="hidden lg:flex items-center">
-          <Link href="/signin" className="mr-7 text-dark hover:opacity-70 dark:text-white">
-            Sign In
-          </Link>
-          <Link href="/signup" className="rounded-sm bg-primary px-8 py-3 text-white transition duration-300 hover:bg-opacity-90">
-            Sign Up
-          </Link>
-          <ThemeToggler />
-        </div>
+            <Link href="/signin" className="mr-7 text-dark hover:opacity-70 dark:text-white">
+              Sign In
+            </Link>
+            <Link href="/signup" className="rounded-sm bg-primary px-8 py-3 text-white transition duration-300 hover:bg-opacity-90">
+              Sign Up
+            </Link>
+            <ThemeToggler />
+          </div>
         </div>
       </div>
     </header>
