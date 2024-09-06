d50322ec33a8338624d3357313db4f5aa8fab22a
Meryem EL KARATI <elkaratimeryem@gmail.com>
2024-09-06 12:32:11 +0200

    Replace email icons with requested ones. Replaced UMR5 with UM5R

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -36,7 +36,10 @@ const Footer = () => {
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24"
                   >
-                    <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2zm0 2v12h16V6H4zm8 6l8-5H4l8 5zm0 2L4 9v8h16v-8l-8 5z" />
+                    <g xmlns="http://www.w3.org/2000/svg"> 
+                      <polyline fill="none" points="4 8.2 12 14.1 20 8.2" stroke="#959cb1" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2"></polyline> 
+                      <rect fill="none" height="14" rx="2" ry="2" stroke="#959cb1" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" width="18" x="3" y="6.5"></rect> 
+                    </g>
                   </svg>
                   <p className="text-base text-body-color dark:text-body-color-dark">
                     Email:{" "}
@@ -175,7 +178,7 @@ const Footer = () => {
             <div className="w-full px-4 lg:w-1/2">
               <div className="my-12 lg:my-16 w-full lg:w-3/4">
                 <h2 className="mb-10 text-xl font-bold text-black dark:text-white">
-                  ENSIAS UMR5
+                  ENSIAS UM5R
                 </h2>
                 <ul>
                   <li className="flex items-center mb-4">
@@ -186,12 +189,7 @@ const Footer = () => {
                       viewBox="0 0 24 24"
                       xmlns="http://www.w3.org/2000/svg"
                     >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M3 12l2-2m0 0l7-7 7 7m-2 2v7a2 2 0 01-2 2H9a2 2 0 01-2-2v-7m10 0l-2 2m-6-2l-2 2"
-                      ></path>
+                      <path xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" d="M11.3103 1.77586C11.6966 1.40805 12.3034 1.40805 12.6897 1.77586L20.6897 9.39491L23.1897 11.7759C23.5896 12.1567 23.605 12.7897 23.2241 13.1897C22.8433 13.5896 22.2103 13.605 21.8103 13.2241L21 12.4524V20C21 21.1046 20.1046 22 19 22H14H10H5C3.89543 22 3 21.1046 3 20V12.4524L2.18966 13.2241C1.78972 13.605 1.15675 13.5896 0.775862 13.1897C0.394976 12.7897 0.410414 12.1567 0.810345 11.7759L3.31034 9.39491L11.3103 1.77586ZM5 10.5476V20H9V15C9 13.3431 10.3431 12 12 12C13.6569 12 15 13.3431 15 15V20H19V10.5476L12 3.88095L5 10.5476ZM13 20V15C13 14.4477 12.5523 14 12 14C11.4477 14 11 14.4477 11 15V20H13Z" fill="#959cb1"></path>
                     </svg>
                     <p className="text-base text-body-color dark:text-body-color-dark">
                       Avenue Mohammed Ben Abdallah Regragui, Madinat Al Irfane, BP 713, Agdal Rabat, Maroc
@@ -204,7 +202,10 @@ const Footer = () => {
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 24 24"
                     >
-                      <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2zm0 2v12h16V6H4zm8 6l8-5H4l8 5zm0 2L4 9v8h16v-8l-8 5z" />
+                      <g xmlns="http://www.w3.org/2000/svg"> 
+                        <polyline fill="none" points="4 8.2 12 14.1 20 8.2" stroke="#959cb1" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></polyline> 
+                        <rect fill="none" height="14" rx="2" ry="2" stroke="#959cb1" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" width="18" x="3" y="6.5"></rect> 
+                      </g>
                     </svg>
                     <p className="text-base text-body-color dark:text-body-color-dark">
                       Email:{" "}
