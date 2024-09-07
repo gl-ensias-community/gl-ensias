e86c9d6f6fbaf36de5258448101f581fe54bee68
EL BOUZIYANI Anas <aaelbouziyani@gmail.com>
2024-09-07 23:51:26 +0100

    Fix warning : Invalid DOM property `stroke-linecap`. Did you mean `strokeLinecap`? and other similar warnings
    based on : https://stackoverflow.com/questions/66685543/reactjs-invalid-dom-property-error-on-svg-elements

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -37,8 +37,8 @@ const Footer = () => {
                     viewBox="0 0 24 24"
                   >
                     <g xmlns="http://www.w3.org/2000/svg"> 
-                      <polyline fill="none" points="4 8.2 12 14.1 20 8.2" stroke="#959cb1" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2"></polyline> 
-                      <rect fill="none" height="14" rx="2" ry="2" stroke="#959cb1" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" width="18" x="3" y="6.5"></rect> 
+                      <polyline fill="none" points="4 8.2 12 14.1 20 8.2" stroke="#959cb1" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"></polyline> 
+                      <rect fill="none" height="14" rx="2" ry="2" stroke="#959cb1" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" width="18" x="3" y="6.5"></rect> 
                     </g>
                   </svg>
                   <p className="text-base text-body-color dark:text-body-color-dark">
@@ -189,7 +189,7 @@ const Footer = () => {
                       viewBox="0 0 24 24"
                       xmlns="http://www.w3.org/2000/svg"
                     >
-                      <path xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" d="M11.3103 1.77586C11.6966 1.40805 12.3034 1.40805 12.6897 1.77586L20.6897 9.39491L23.1897 11.7759C23.5896 12.1567 23.605 12.7897 23.2241 13.1897C22.8433 13.5896 22.2103 13.605 21.8103 13.2241L21 12.4524V20C21 21.1046 20.1046 22 19 22H14H10H5C3.89543 22 3 21.1046 3 20V12.4524L2.18966 13.2241C1.78972 13.605 1.15675 13.5896 0.775862 13.1897C0.394976 12.7897 0.410414 12.1567 0.810345 11.7759L3.31034 9.39491L11.3103 1.77586ZM5 10.5476V20H9V15C9 13.3431 10.3431 12 12 12C13.6569 12 15 13.3431 15 15V20H19V10.5476L12 3.88095L5 10.5476ZM13 20V15C13 14.4477 12.5523 14 12 14C11.4477 14 11 14.4477 11 15V20H13Z" fill="#959cb1"></path>
+                      <path xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" d="M11.3103 1.77586C11.6966 1.40805 12.3034 1.40805 12.6897 1.77586L20.6897 9.39491L23.1897 11.7759C23.5896 12.1567 23.605 12.7897 23.2241 13.1897C22.8433 13.5896 22.2103 13.605 21.8103 13.2241L21 12.4524V20C21 21.1046 20.1046 22 19 22H14H10H5C3.89543 22 3 21.1046 3 20V12.4524L2.18966 13.2241C1.78972 13.605 1.15675 13.5896 0.775862 13.1897C0.394976 12.7897 0.410414 12.1567 0.810345 11.7759L3.31034 9.39491L11.3103 1.77586ZM5 10.5476V20H9V15C9 13.3431 10.3431 12 12 12C13.6569 12 15 13.3431 15 15V20H19V10.5476L12 3.88095L5 10.5476ZM13 20V15C13 14.4477 12.5523 14 12 14C11.4477 14 11 14.4477 11 15V20H13Z" fill="#959cb1"></path>
                     </svg>
                     <p className="text-base text-body-color dark:text-body-color-dark">
                       Avenue Mohammed Ben Abdallah Regragui, Madinat Al Irfane, BP 713, Agdal Rabat, Maroc
@@ -203,8 +203,8 @@ const Footer = () => {
                       viewBox="0 0 24 24"
                     >
                       <g xmlns="http://www.w3.org/2000/svg"> 
-                        <polyline fill="none" points="4 8.2 12 14.1 20 8.2" stroke="#959cb1" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></polyline> 
-                        <rect fill="none" height="14" rx="2" ry="2" stroke="#959cb1" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" width="18" x="3" y="6.5"></rect> 
+                        <polyline fill="none" points="4 8.2 12 14.1 20 8.2" stroke="#959cb1" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"></polyline> 
+                        <rect fill="none" height="14" rx="2" ry="2" stroke="#959cb1" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" width="18" x="3" y="6.5"></rect> 
                       </g>
                     </svg>
                     <p className="text-base text-body-color dark:text-body-color-dark">
