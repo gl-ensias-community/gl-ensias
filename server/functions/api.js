d929d64cbe1c3819062238566ea22e072c8738e2
Ahmed Idrissi <83851898+ahmedidrissi@users.noreply.github.com>
2024-09-22 17:38:25 +0100

    Refactor server configuration and deployment

--- server/functions/api.js
+++ server/functions/api.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const { sendContactEmail } = require("./actions/mail");
-const { getMessages, sendMessage } = require("./actions/contact");
+const serverless = require("serverless-http");
+const { sendContactEmail } = require("../actions/mail");
+const { getMessages, sendMessage } = require("../actions/contact");
 require("dotenv/config");
 
 const app = express();
 const port = process.env.PORT || 5000;
 const origin = process.env.ORIGIN || "http://localhost:3000";
-
+const baseUrl = '/.netlify/functions/api/v1';
 const corsOptions = {
   origin: origin,
   credentials: true,
@@ -19,11 +20,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors(corsOptions));
 
-app.get("/api/v1", (req, res) => {
+app.get(`${baseUrl}/`, (req, res) => {
   res.status(200).json({ message: "Welcome to GL ENSIAS API" });
 });
 
-app.post("/api/v1/contact/new", async (req, res) => {
+app.post(`${baseUrl}/contact/new`, async (req, res) => {
   const contactData = req.body;
   name = contactData.name;
   to = contactData.email;
@@ -55,7 +56,7 @@ app.post("/api/v1/contact/new", async (req, res) => {
   }
 });
 
-app.get("/api/v1/contact/list", async (req, res) => {
+app.get(`${baseUrl}/contact/list`, async (req, res) => {
   try {
     const messages = await getMessages();
     if (messages.error) {
@@ -68,6 +69,12 @@ app.get("/api/v1/contact/list", async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
+const handler = serverless(app);
+
+module.exports.handler = async (event, context) => {
+  return await handler(event, context);
+};
